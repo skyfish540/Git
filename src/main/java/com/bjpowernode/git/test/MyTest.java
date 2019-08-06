5ddb7e69fb64ebadab79cdda807b91ebf5edc621
skyfish540 <542660032@qq.com>
2019-08-06 14:30:42 +0800

    testGit提交整个项目

---
+++ src/main/java/com/bjpowernode/git/test/MyTest.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.git.test;
+
+/**
+ *
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111");
+        System.out.println("222222222222222222");
+
+    }
+}
---
+++ src/main/java/com/bjpowernode/model/User.java
@@ -0,0 +1,28 @@
+package com.bjpowernode.model;
+
+/**
+ *
+ */
+public class User {
+    private String userName;
+    private Integer age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public User() {
+    }
+}
